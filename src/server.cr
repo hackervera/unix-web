6d86ee9bd95f70916bbc37b41463c2f3b8660dfa
Tyler Gillies <tjgillies@gmail.com>
2017-05-20 07:12:29 -0700

    remove vestigial code

--- src/server.cr
+++ src/server.cr
@@ -7,9 +7,6 @@ config.each do |route, handler|
     get route do |env|
         error = IO::Memory.new
         output = IO::Memory.new
-        params = env.params.query.map do |k, v|
-            "#{k}=\"#{v}\""
-        end.join " "
         env.response.content_type = "text/plain"
         process = Process.run(command: handler.to_s, env: env.params.query.to_h, error: error, output: output)
         error.to_s.empty? ?  output.to_s : error.to_s 
