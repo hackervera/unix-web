f96fed1791da6bec4e6ba0abfa1f586f75efec40
Tyler Gillies <tjgillies@gmail.com>
2017-05-20 07:11:15 -0700

    use better processing

--- src/server.cr
+++ src/server.cr
@@ -2,12 +2,17 @@ require "kemal"
 require "toml"
 
 config = TOML.parse_file("config.toml")
+
 config.each do |route, handler|
     get route do |env|
+        error = IO::Memory.new
+        output = IO::Memory.new
         params = env.params.query.map do |k, v|
-            "#{k}=#{v}"
+            "#{k}=\"#{v}\""
         end.join " "
-        `#{params} #{handler}`
+        env.response.content_type = "text/plain"
+        process = Process.run(command: handler.to_s, env: env.params.query.to_h, error: error, output: output)
+        error.to_s.empty? ?  output.to_s : error.to_s 
     end
 end
 
